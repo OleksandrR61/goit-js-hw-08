6fc729b32031dfbd926d3ff2e7781e7e41a8bc03
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2022-10-31 22:02:29 +0200

    HW-JS#8v0.1
    Task 1 complite

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(item => {
+        return `<a class="gallery__item" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>`;
+    }).join('');
+}
+
+document.querySelector(".gallery").insertAdjacentHTML("beforeend", createGallery(galleryItems));
+
+new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
