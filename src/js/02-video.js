fef59c0be15417cb9ca098dfb901ae624d530a19
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2022-11-01 00:09:39 +0200

    HW-JS#8v0.2
    Complite task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function saveCurrentTime({seconds}) {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+
+try {
+    player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+} catch (error) {
+    console.error("Video playback error: ", error.message);
+};
+
+player.on('timeupdate', Throttle(saveCurrentTime, 1000));
