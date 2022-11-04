4f978247a64ec1560eb61dc3abf88daa30f2495c
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2022-11-04 03:03:45 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,6 @@ formRef.email.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email |
 formRef.message.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message || "";
 
 formRef.addEventListener('input', throttle(() => {
-    console.log(formRef.email.value, " ", formRef.message.value);
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({email: formRef.email.value, message: formRef.message.value}));
 }, 500));
 
