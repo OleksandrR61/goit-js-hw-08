ea0b67c05f2b1cebb9a21732b369d2370d581fa4
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2022-11-04 03:01:41 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,12 +6,12 @@ const LOCALSTORAGE_KEY = "feedback-form-state"
 formRef.email.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email || "";
 formRef.message.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message || "";
 
-formRef.querySelector('input', throttle(() => {
+formRef.addEventListener('input', throttle(() => {
     console.log(formRef.email.value, " ", formRef.message.value);
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({email: formRef.email.value, message: formRef.message.value}));
 }, 500));
 
-formRef.querySelector('submit', (event) => {
+formRef.addEventListener('submit', (event) => {
     event.preventDefault();
 
     console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
