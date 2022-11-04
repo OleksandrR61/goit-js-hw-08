19a6962f8cc8540e5cdfcd7b4a986837834063e1
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2022-11-04 02:28:03 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = "feedback-form-state"
+
+function getStorage(storageKey) {
+    if (!JSON.parse(localStorage.getItem(storageKey))) {
+        return {};
+    }
+    return JSON.parse(localStorage.getItem(storageKey));
+}
+
+function updateFormField(formField, storageField) {
+    if (storageField) {
+        formField.value = storageField;
+    };
+}
+
+function updateForm(form, storageKey) {
+    updateFormField(form.elements.email, getStorage(storageKey).email);
+    updateFormField(form.elements.message, getStorage(storageKey).message);
+}
+
+function saveData(event, storageKey) {
+    let {email, message} = event.currentTarget.elements;
+
+    localStorage.setItem(storageKey, JSON.stringify({email: email.value, message: message.value}));
+}
+
+function clearData(event, storageKey) {
+    event.preventDefault();
+
+    console.log(getStorage(storageKey));
+
+    event.currentTarget.reset();
+    localStorage.clear();
+}
+
+updateForm(formRef, LOCALSTORAGE_KEY);
+
+formRef.addEventListener('input', (event) => throttle(saveData(event, LOCALSTORAGE_KEY), 500));
+formRef.addEventListener('submit', clearData(LOCALSTORAGE_KEY));
